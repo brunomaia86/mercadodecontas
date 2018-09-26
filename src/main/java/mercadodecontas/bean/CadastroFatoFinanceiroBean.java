c83a8ca95160f54bbcef6debacac89f1651fe38f
bruno.maia <bruno.maia@MATRIZ-300677.hapvida.com.br>
2018-09-26 16:30:11 -0300

    ajustes

--- src/main/java/mercadodecontas/bean/CadastroFatoFinanceiroBean.java
+++ src/main/java/mercadodecontas/bean/CadastroFatoFinanceiroBean.java
@@ -1,5 +1,19 @@
 package mercadodecontas.bean;
 
-public class CadastroFatoFinanceiroBean {
+import java.io.Serializable;
 
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+@Named
+@ViewScoped
+public class CadastroFatoFinanceiroBean implements Serializable {
+
+	private static final long serialVersionUID = -7476328032330398810L;
+
+	/*@Inject
+	private CadastroFatoFinanceiroDAO cadastroFatoFinanceiroDAO;
+
+	@Inject
+	private CrudDao<FatoFinanceiro, Long> genericDAO;*/
 }
--- src/main/java/mercadodecontas/dao/CadastroFatoFinanceiroDAO.java
+++ src/main/java/mercadodecontas/dao/CadastroFatoFinanceiroDAO.java
@@ -1,5 +1,25 @@
 package mercadodecontas.dao;
 
-public class CadastroFatoFinanceiroDAO {
+import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.ejb.Stateless;
+
+import mercadodecontas.model.FatoFinanceiro;
+
+@Stateless
+public class CadastroFatoFinanceiroDAO extends CrudDao<FatoFinanceiro, Long> {
+
+	private static final long serialVersionUID = -4400316965873859659L;
+
+	List<FatoFinanceiro> listaFatosFinanceiros;
+	
+	@PostConstruct
+	public void init() {
+		
+	}
+	
+	public List<FatoFinanceiro> buscarTodos() {
+		
+	}
 }
